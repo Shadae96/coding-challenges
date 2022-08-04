001be43b709bbca2d5a201dd457d92d4698c06db
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-08-04 00:12:16 -0500

    updated primenumber checker

--- 8-2-2022/PrimeNumberChecker.java
+++ 8-2-2022/PrimeNumberChecker.java
@@ -1,42 +1,28 @@
-/**
- * PrimeNumberChecker
- */
 import java.util.Scanner;
 
-public class PrimeNumberChecker {
-
-    public static void main(Integer[] args) {
-        
-    Scanner input = new Scanner(System.in);
-
-    System.out.print("enter a number: ");
-
-
-    int num = input.nextLine();
-
-   if (isPrime(num)) {System.out.println("the number "+ num + " you entered is prime");
-
-
-    }
-
-    public static boolean isPrime(int n) {
-        if(n <= 1){
-            if (n= 1){
-                return false
-            }
-
-            for (int i =2; i < Math.sqrt(n); i++)
-            {
-                if(n % i == 0) {
-                    return false;
-                }
-            }
-        }
-
-        return false;
-        
-    }
-
-
-    }
+public class PrimeChecker {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int n = s.nextInt();
+if (isPrime(n)) {
+System.out.println(n + " is a prime number.");
+} else {
+System.out.println(n + " is not a prime number.");
 }
+}
+
+
+public static boolean isPrime(int n) {
+if (n <= 1) {
+return false;
+}
+for (int i = 2; i < Math.sqrt(n); i++) {
+if (n % i == 0) {
+return false;
+}
+}
+return true;
+}
+}
+
